7d6ccbe4c38fa52e9c76dc9bbc4f474552a7d9ce
Obi1Ken0bi <puzikov.artem@gmail.com>
2022-03-06 12:55:34 +0300

    daily lesson 6

---
+++ 589. N-ary Tree Preorder Traversal/src/Solution.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(root!=null) {
+            result.add(root.val);
+            for (Node n : root.children
+            ) {
+                result.addAll(preorder(n));
+            }
+        }
+        return result;
+    }
+}
