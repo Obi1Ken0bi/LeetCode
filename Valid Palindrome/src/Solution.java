ec69e4ab182f3729fe7e1a0b85b7dbb3614413eb
Obi1Ken0bi <puzikov.artem@gmail.com>
2022-04-08 14:02:54 +0300

    main

---
+++ Valid Palindrome/src/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.replaceAll("[^a-zA-Z\\d]","");
+        s=s.toLowerCase();
+        if(s.equals(""))
+            return true;
+        int i=0;
+        int j=s.length()-1;
+        while (i<=j){
+            if(s.charAt(i)!=s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution=new Solution();
+        System.out.println(solution.isPalindrome("aa"));
+    }
+}
