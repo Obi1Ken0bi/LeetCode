bba2d2232f8700310d23bee19a740a77738b3e89
Obi1Ken0bi <puzikov.artem@gmail.com>
2022-04-13 18:45:24 +0300

    main

---
+++ Delete Node in a Linked List/src/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+        public void deleteNode(ListNode node) {
+
+                ListNode nextNode=node.next;
+                node.val = nextNode.val;
+                node.next = nextNode.next;
+                node=node.next;
+
+
+
+        }
+
+}
